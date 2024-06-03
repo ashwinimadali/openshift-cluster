d9ac7c38e84d6fc6e3e4e33f3eaedefaa269bf68
Shashikant Patil <shashikantpatil2727@outlook.com>
2024-06-03 17:10:21 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "openshiftResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "openshiftstorage123"  # Updated with a valid name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "openshift"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.example.name
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.example.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.example.name
+}
